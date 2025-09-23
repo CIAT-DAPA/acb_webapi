a95e09d677f070df5299a1546c2a3a18f3d2a551
Victor Hernandez <victor.993_@hotmail.com>
2025-09-23 13:23:46 -0500

    update validate tockens

--- src/auth/token_validation_router.py
+++ src/auth/token_validation_router.py
@@ -5,6 +5,8 @@ import requests
 import os
 from dotenv import load_dotenv
 
+from auth.access_utils import get_current_user
+
 load_dotenv()
 
 router = APIRouter(tags=["Authentication"], prefix="/auth")
@@ -13,44 +15,6 @@ security = HTTPBearer()
 
 @router.get("/token/validate", summary="Validate a keycloak token")
 def validate_local_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    token = credentials.credentials
-    unverified_header = jwt.get_unverified_header(token)
-
-    KEYCLOAK_URL = os.getenv("KEYCLOAK_URL")
-    REALM_NAME = os.getenv("KEYCLOAK_REALM")
-    CLIENT_ID = os.getenv("KEYCLOAK_CLIENT_ID")
-
-    jwks_url = f"{KEYCLOAK_URL}/realms/{REALM_NAME}/protocol/openid-connect/certs"
-    response = requests.get(jwks_url)
-    if response.status_code != 200:
-        raise HTTPException(status_code=500, detail="Error fetching JWKS from Keycloak")
-    jwks = response.json()
-
-    key = next((k for k in jwks["keys"] if k["kid"] == unverified_header["kid"]), None)
-    if not key:
-        raise HTTPException(status_code=401, detail="Public key not found")
-
-    try:
-        payload = jwt.decode(
-            token,
-            key,
-            algorithms=[unverified_header["alg"]],
-            audience="account",
-            issuer=f"{KEYCLOAK_URL}/realms/{REALM_NAME}",
-        )
-
-        filtered_payload = {
-            k: v for k, v in payload.items()
-            if k not in ["realm_access", "allowed-origins", "resource_access"]
-        }
-
-        client_roles = payload.get("resource_access", {}).get("GanabosquesWeb", {}).get("roles", [])
-        filtered_payload["client_roles"] = client_roles
-
-        return {"valid": True, "payload": filtered_payload}
-
-    except ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Expired token")
-    except JWTError as e:
-        raise HTTPException(status_code=401, detail=f"Invalid Token: {str(e)}")
+    current_user = get_current_user(credentials)
+    return {"valid": True, "payload": current_user}
 
